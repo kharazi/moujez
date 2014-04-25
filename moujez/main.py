6a016741038cea10a707ed58263c7a422ef33fc8
vahid <vahid@kharazi.net>
2014-04-26 00:30:22 +0430

    few change in logo and graphic

--- moujez/main.py
+++ moujez/main.py
@@ -21,7 +21,7 @@ def add_numbers():
     
     a = request.args.get('a', 0, type=str)
     summarizer = Summarizer()
-    a = u"http://www.asriran.com/fa/news/332468/%D9%BE%DB%8C%D8%A7%D9%85-%D8%B3%DB%8C%D8%AF-%D9%85%D8%AD%D9%85%D8%AF-%D8%AE%D8%A7%D8%AA%D9%85%DB%8C-%D8%A8%D9%87-%DA%A9%D9%86%DA%AF%D8%B1%D9%87-%D9%8A%DA%A9-%D8%AD%D8%B2%D8%A8"
+    # a = u"http://www.asriran.com/fa/news/332468/%D9%BE%DB%8C%D8%A7%D9%85-%D8%B3%DB%8C%D8%AF-%D9%85%D8%AD%D9%85%D8%AF-%D8%AE%D8%A7%D8%AA%D9%85%DB%8C-%D8%A8%D9%87-%DA%A9%D9%86%DA%AF%D8%B1%D9%87-%D9%8A%DA%A9-%D8%AD%D8%B2%D8%A8"
     page = Fetch(a)
     if page.service == 'iransamane':
         service = IranSamane(page.page)
